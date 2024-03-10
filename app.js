1f6f065a13e5e63bfc29f3e5c96bb3372c1f8444
PARTH CHAWLA <82279033+sponge-bobs-square-pants@users.noreply.github.com>
2024-03-10 21:55:35 +0530

    finding looping

--- app.js
+++ app.js
@@ -38,34 +38,21 @@ app.get('/api/auth/Token', async (req, res) => {
 
 app.get('/api/auth/newToken', async (req, res) => {
   console.log('I m getting called');
-  try {
-    console.log('I m getting called1');
-    // const zohoData = {
-    //   refresh_token:
-    //     '1000.e571752c5d9e9b94044542621327ae38.2fc29af8ab61fe78a0ace94fb77727bf',
-    //   client_id: '1000.IJSUXC1199E5GTAI0GHEJE879XP8YW',
-    //   client_secret: '0cee3a6d8834f049675f471f2f0d284ecab590adc5',
-    //   scope:
-    //     'ZohoCRM.modules.all,Desk.tickets.ALL,Desk.contacts.ALL,ZohoSubscriptions.fullaccess.all,Desk.contacts.UPDATE',
-    //   grant_type: 'refresh_token',
-    // };
-    // const ZohoRequest = await axios.post(
-    //   'https://accounts.zoho.in/oauth/v2/token?refresh_token=1000.188fa35ef745478a1a52062d37a453f4.7d990b72adc6daa21c24b80428896633&client_id=1000.IJSUXC1199E5GTAI0GHEJE879XP8YW&client_secret=0cee3a6d8834f049675f471f2f0d284ecab590adc5&scope=ZohoCRM.modules.all,Desk.tickets.ALL,Desk.contacts.ALL,ZohoSubscriptions.fullaccess.all,Desk.contacts.UPDATE&grant_type=refresh_token'
-    // );
+  console.log('I m getting called1');
+  // const ZohoRequest = await axios.post(
+  //   'https://accounts.zoho.in/oauth/v2/token?refresh_token=1000.188fa35ef745478a1a52062d37a453f4.7d990b72adc6daa21c24b80428896633&client_id=1000.IJSUXC1199E5GTAI0GHEJE879XP8YW&client_secret=0cee3a6d8834f049675f471f2f0d284ecab590adc5&scope=ZohoCRM.modules.all,Desk.tickets.ALL,Desk.contacts.ALL,ZohoSubscriptions.fullaccess.all,Desk.contacts.UPDATE&grant_type=refresh_token'
+  // );
 
-    // const { access_token } = ZohoRequest.data;
-    // console.log('zoho token has been called', access_token);
-    // const existingToken = await Token.findOne();
-    // if (!existingToken) {
-    //   return res.status(404).json({ error: 'Token not found' });
-    // }
-    // existingToken.BearerToken = access_token;
-    // await existingToken.save();
-    // const FullToken = 'Zoho-oauthtoken ' + access_token;
-    // return res.json({ token: FullToken });
-  } catch (error) {
-    return res.status(500).json({ error: 'Internal Server Error' });
-  }
+  // const { access_token } = ZohoRequest.data;
+  // console.log('zoho token has been called', access_token);
+  // const existingToken = await Token.findOne();
+  // if (!existingToken) {
+  //   return res.status(404).json({ error: 'Token not found' });
+  // }
+  // existingToken.BearerToken = access_token;
+  // await existingToken.save();
+  // const FullToken = 'Zoho-oauthtoken ' + access_token;
+  // return res.json({ token: FullToken });
 });
 // app.post('/api/auth/Token', async (req, res) => {
 //   try {
